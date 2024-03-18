d60460acb57d22847cc372076d901d9517b9a50f
botatodev <botatodev@gmail.com>
2024-03-18 04:47:29 +0300

    added aliases and typedef size_t to index_t in Base.h

--- Route/src/Route/Bases.h
+++ Route/src/Route/Bases.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <stdint.h>
+#include <array>
+#include <vector>
+#include <tuple>
 
 namespace route
 {
+	using std::array;
+	using std::vector;
+	using std::tuple;
+
+	typedef size_t index_t;
 }
