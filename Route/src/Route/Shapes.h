1ca28e5e40cf93a0438c5c6aec1198f55b535182
botatodev <botatodev@gmail.com>
2024-03-25 01:03:03 +0300

    added Shapes.h

---
+++ Route/src/Route/Shapes.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace route
+{
+	enum class Shape2D
+	{
+		Triangle,
+		Circle,
+		Rectangle,
+		Line
+	};
+	enum class Shape2D
+	{
+		Cube,
+		Sphere,
+		Pyrimid,
+		Plane,
+		Tupe,
+	};
+}
