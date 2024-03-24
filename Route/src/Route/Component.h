814999c111a95bc5f7edd2e228e60f29447cd315
botatodev <botatodev@gmail.com>
2024-03-25 01:02:20 +0300

    changes to Component.h/cpp

--- Route/src/Route/Component.h
+++ Route/src/Route/Component.h
@@ -1,36 +1,26 @@
 #pragma once
 #include "Bases.h"
 #include "Vector.h"
-#include "Boxed.h"
 
 namespace route
 {
-	class BaseObject;
+	class Object;
 
-	namespace comps
+	struct Component
 	{
-		_INTERFACE Component
-		{
-			virtual void added( BaseObject &object ) = 0;
-			virtual void removed( BaseObject &object ) = 0;
-			virtual void update( BaseObject &object ) = 0;
-			virtual void draw( const BaseObject &object ) const = 0;
+		virtual void added( Object &object ) = 0;
+		virtual void removed( Object &object ) = 0;
+		virtual void update( Object &object ) = 0;
+		virtual void draw( const Object &object ) const = 0;
 
-		};
+	};
 
-		struct ShapeComponent : public Component
-		{
-		};
+	struct ShapeComponent : public Component
+	{
+	};
 
-
-
-
-		static constexpr size_t component_max_size = 128;
-	}
 	template <typename _Ty>
 	_INLINE_VAR constexpr bool is_component_v =
-		std::is_same_v<comps::Component, _Ty> || std::is_base_of_v<comps::Component, _Ty>;
-
-	using component_boxed = boxed<comps::Component, placement_block<comps::component_max_size>>;
-
+		std::is_same_v<Component, _Ty> || std::is_base_of_v<Component, _Ty>;
+	using component = Component;
 }
