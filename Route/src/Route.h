0416390624e129187026d444a06cde31f4c42237
botatodev <botatodev@gmail.com>
2024-03-28 06:13:40 +0300

    Version to Route.h

--- Route/src/Route.h
+++ Route/src/Route.h
@@ -1,8 +1,15 @@
 #ifndef _H_ROUTE
 #define _H_ROUTE
 
-#include "Route/Engine.h"
-#include "Route/Window.h"
+#include "Route/Application.h"
+
+namespace route
+{
+
+	_INLINE_VAR constexpr uint32_t VersionMajor = 0;
+	_INLINE_VAR constexpr uint32_t VersionMinor = 1;
+
+}
 
 
 #endif // !_H_ROUTE
