fb964d19ae0939f80de6e3da6f12857cd2c70419
botatodev <botatodev@gmail.com>
2024-03-28 06:11:30 +0300

    added the resource server

---
+++ Route/src/Route/ResourceServer.cpp
@@ -0,0 +1,241 @@
+#include "pch.h"
+#include "../pch.h"
+#include "ResourceServer.h"
+#include <list>
+#include "Image.h"
+
+
+namespace route
+{
+	template ResourceServer<Image>;
+
+	static constexpr RID ChunkBShift = 20;
+	static constexpr RID ChunkMask = ~((1u << ChunkBShift) - 1); // maximum possible number of chunks is 4096
+	static constexpr RID ElementMask = ~ChunkMask;
+
+	struct RIndex
+	{
+		uint16_t chunk;
+		uint16_t element;
+	};
+
+	template<typename _Ty>
+	typename ResourceServer<_Ty>::Internal *ResourceServer<_Ty>::s_internal = nullptr;
+
+	enum ElementFlags
+	{
+		EFlag_None = 0,
+		EFlag_Init = 1,
+	};
+
+	template<typename _Ty>
+	struct ResourceServer<_Ty>::Internal {
+
+
+
+		struct Element
+		{
+			static constexpr size_t value_size = sizeof( _Ty );
+			using memory_element = uint8_t;
+			static constexpr size_t memory_count =
+				value_size / sizeof( memory_element ) + (value_size % sizeof( memory_element ) ? 1 : 0);
+			using memory_block = memory_element[ memory_count ];
+
+			inline _Ty &value() {
+				return *reinterpret_cast<_Ty *>(mem);
+			}
+
+			ElementFlags flags{};
+			array<resource_name_char, ResourceNameMaxLn> name{};
+			memory_block mem; // <- don't init on default!
+		};
+
+		static constexpr size_t ChunkBufferLength = 256;
+
+		struct Chunk
+		{
+		public:
+
+			inline bool is_locked() const {
+				return m_locked;
+			}
+
+			inline uintptr_t counter() const {
+				return m_elements_counter;
+			}
+
+			inline void dec_counter() {
+				m_elements_counter--;
+				if (!m_elements_counter)
+				{
+					m_locked = false;
+				}
+			}
+
+			inline void inc_counter() {
+				m_elements_counter++;
+				if (m_elements_counter >= elements.size())
+				{
+					m_locked = true;
+				}
+			}
+
+			// a chunk becomes locked when it's filled
+			// also, a chuck unlocks when it's empty
+			array<Element, ChunkBufferLength> elements{};
+		private:
+			bool m_locked = false;
+			uintptr_t m_elements_counter = 0;
+		};
+
+		inline index_t first_unlocked_chunk() const {
+			for (index_t i = 0; i < chunks.size(); i++)
+			{
+				if (!chunks[ i ].is_locked())
+				{
+					return i;
+				}
+			}
+			return npos;
+		}
+
+		inline index_t create_chunk() {
+			chunks.emplace_back();
+			return chunks.size() - 1;
+		}
+
+		// gets the first unlocked chunk, if non are found; create one
+		inline index_t unlocked_chunk_or_create() {
+			const index_t first = first_unlocked_chunk();
+			if (first == npos)
+			{
+				return create_chunk();
+			}
+			return first;
+		}
+
+		inline RIndex get_rindex( const RID rid ) const {
+			return { static_cast<uint16_t>(rid >> ChunkBShift), static_cast<uint16_t>(rid & ElementMask) };
+		}
+
+		inline RID to_rid( const RIndex rindex ) const {
+			return ((rindex.chunk << ChunkBShift) | (rindex.element & ElementMask));
+		}
+
+		inline _Ty &get_resource( const RID rid ) {
+			const auto rindex = get_rindex( rid );
+			return *reinterpret_cast<_Ty *>(chunks[ rindex.chunk ].elements[ rindex.element ].mem);
+		}
+
+		size_t ref_counter;
+		vector<Chunk> chunks{};
+	};
+
+
+
+	template<typename _Ty>
+	typename ResourceServer<_Ty>::resource_type &ResourceServer<_Ty>::get_resource( RID rid ) {
+		return s_internal->get_resource( rid );
+	}
+
+	template<typename _Ty>
+	RID ResourceServer<_Ty>::add_resource( const resource_type &resource ) {
+		const index_t chunk_index = s_internal->unlocked_chunk_or_create();
+
+		typename Internal::Chunk &chunk = s_internal->chunks[ chunk_index ];
+		const index_t add_index = chunk.counter();
+
+		// after the last filled element is a filled element????
+		// something fucked up, kill me
+		if (chunk.elements[ add_index ].flags & EFlag_Init)
+		{
+			return RIDnpos;
+		}
+
+		chunk.inc_counter(); /* can we afford to check for de-syncs?? */
+		new (chunk.elements[ add_index ].mem) resource_type( resource );
+		chunk.elements[ add_index ].flags = EFlag_Init;
+
+		return s_internal->to_rid( { static_cast<uint16_t>(chunk_index), static_cast<uint16_t>(add_index) } );
+	}
+
+	template<typename _Ty>
+	void ResourceServer<_Ty>::pop_resource( RID rid ) {
+		const RIndex rindex = s_internal->get_rindex( rid );
+
+		if (rindex.chunk >= s_internal->chunks.size())
+			return;
+
+		if (rindex.element >= Internal::ChunkBufferLength)
+			return;
+
+		typename Internal::Element &element = s_internal->chunks[ rindex.chunk ].elements[ rindex.element ];
+		element.value().~resource_type();
+		element.flags = EFlag_None;
+
+		s_internal->chunks[ rindex.chunk ].dec_counter();
+	}
+
+	template<typename _Ty>
+	void ResourceServer<_Ty>::set_resource_name( RID rid, const resource_name_char *name ) {
+		const RIndex rindex = s_internal->get_rindex( rid );
+		(void)strncpy_s(
+			s_internal->chunks[ rindex.chunk ].elements[ rindex.element ].name.data(),
+			ResourceNameMaxLn,
+			name,
+			_TRUNCATE
+		);
+	}
+
+	template<typename _Ty>
+	errno_t ResourceServer<_Ty>::open() {
+		if (s_internal)
+		{
+			s_internal->ref_counter++;
+			return 0;
+		}
+
+		s_internal = new Internal();
+		s_internal->ref_counter = 1;
+		return 0;
+	}
+
+	template<typename _Ty>
+	bool ResourceServer<_Ty>::close() {
+		// no internals
+		if (!s_internal)
+		{
+			char buf[ 256 ]{};
+			(void)sprintf_s( buf, "%s already closed!", typeid(ResourceServer<_Ty>).name() );
+			std::_Xruntime_error( buf );
+			return false;
+		}
+
+		// was the ref_counter zero to overflow? then why isn't it deleted??
+		if (--s_internal->ref_counter == npos)
+		{
+			char buf[ 256 ]{};
+			(void)sprintf_s(
+				buf,
+				"%s has a corrupted internals ref_counter: %llu",
+				typeid(ResourceServer<_Ty>).name(),
+				s_internal->ref_counter
+			);
+			std::_Xruntime_error( buf );
+			return false;
+		}
+
+		// no more references to this resource server
+		if (!s_internal->ref_counter)
+		{
+			delete s_internal;
+			s_internal = nullptr;
+
+			// the internals are deleted
+			return true;
+		}
+
+		// the internals are still there
+		return false;
+	}
+}
---
+++ Route/src/Route/ResourceServer.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "Bases.h"
+
+namespace route
+{
+	using RID = uint32_t;
+	_INLINE_VAR constexpr auto RIDnpos = RID( -1 );
+
+	class Application;
+	class Renderer;
+
+	template <typename _Ty>
+	class ResourceServer
+	{
+		friend Application;
+		friend Renderer;
+	public:
+		using resource_type = _Ty;
+		using resource_name_char = char;
+		static constexpr size_t ResourceNameMaxLn = 128;
+		struct Internal;
+
+		static resource_type &get_resource( RID rid );
+		static RID add_resource( const resource_type &resource );
+		static void pop_resource( RID rid );
+
+		/// @brief 
+		/// @param rid the resource id
+		/// @param name the name to be set, read until encountering a null-termination or reached the ResourceNameMaxLn
+		static void set_resource_name( RID rid, const resource_name_char *name );
+
+	private:
+
+		/// @note increments the ref-counter if the server is already running
+		static errno_t open();
+		/// @returns true if the server closed, false otherwise (decremented ref counter)
+		static bool close();
+	private:
+		static Internal *s_internal;
+	};
+
+
+
+}
