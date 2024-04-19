54c697ed4fe5234ebbea23693d3dcaf2fcdcb568
botatodev <botatodev@gmail.com>
2024-04-20 00:54:05 +0300

    changes to the texture class, added TextureFormat enum

--- Route/src/Route/Texture.h
+++ Route/src/Route/Texture.h
@@ -12,6 +12,27 @@ namespace route
 		CubeMap,
 	};
 
+	enum class TextureFormat
+	{
+		Invalid = -1,
+
+		RGB8,
+		RGB16,
+		RGBA8,
+		RGBA16,
+		RGBA32,
+		RGBA16F,
+		RGBA32F,
+
+		BGR8,
+		BGR16,
+		BGRA8,
+		BGRA16,
+		BGRA32,
+		BGRA16F,
+		BGRA32F,
+	};
+
 	typedef vpid_t TextureID;
 
 	class Texture : public Resource
@@ -22,5 +43,8 @@ namespace route
 	private:
 		TextureID m_id;
 		TextureType m_type;
+		TextureFormat m_format;
+		Vec3u m_extent;
+		uint32_t m_mip_levels;
 	};
 }
