0ef413bbf4449d6868d235fd4b8f6ae4fce9d065
botatodev <botatodev@gmail.com>
2024-04-20 20:41:07 +0300

    a bunch of route macros

--- Route/src/pch.h
+++ Route/src/pch.h
@@ -19,6 +19,16 @@ namespace route
 
 using namespace route;
 
+// static inlined constexpr function defintion prefix
+#define RT_FDEF_SICX static inline constexpr
+// static force-inlined constexpr function definition prefix
+#define RT_FDEF_SFIC static FORCE_INLINE constexpr
+// Static Compile Time Only function defintion prefix
+#define RT_FDEF_SCTO static inline consteval
+
+// defining this will make the resource manger check the RIDs when getting resources
+#define RT_RID_CHECKS
+
 #define SDL_FNCHECK(call) \
 { int err = (call); if (err) { char buf[256]{}; sprintf_s(buf, std::size(buf), "Failed to call " #call " at " __FILE__ ":%u, returned %d", __LINE__, err); Logger::write( buf, LogLevel::Error ); } }
 
