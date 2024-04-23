0b36337ca7866b469550b76574db2e5be27d39ff
botatodev <botatodev@gmail.com>
2024-04-23 16:51:05 +0300

    add a renderer shader use with a render demo

--- Route/src/Route/Renderer.cpp
+++ Route/src/Route/Renderer.cpp
@@ -2,13 +2,35 @@
 #include "../pch.h"
 #include "Logger.h"
 #include "Renderer.h"
+#include "StorageBuffer.h"
+
+
 
 
 namespace route
 {
 	class Renderer::State
 	{
+	public:
 
+		State() {
+			auto vertex_shader =
+				"layout (location = 0) in vec4 pos; out vec3 clr;"
+				"void main() { gl_Position = pos; clr = vec3((pos.xy + vec2(1)) / 2.0, 0.5); }"
+				;
+			auto frag_shader =
+				"in vec3 clr; layout (location = 0) out vec4 Color;"
+				"void main() { Color = vec4(clr, 1.0); }"
+				;
+			shader = Shader( vertex_shader, frag_shader );
+
+		}
+
+		~State() {
+		}
+
+		Shader shader;
+		Vec2u last_window_size;
 	};
 
 	Renderer::Renderer( Window &window ) : m_window{ window } {
@@ -19,27 +41,39 @@ namespace route
 			Logger::write( "Failed to create context for Renderer", LogLevel::Error );
 			return;
 		}
-		
+
 		m_state = new State{};
 	}
 
 	Renderer::~Renderer() {
 		const auto current_context = OpenGL::get_context();
 		const auto current_window = OpenGL::get_context_window();
+
 		OpenGL::set_context( m_window.m_handle, m_context );
 		delete m_state;
 		OpenGL::delete_context( m_context );
+
 		OpenGL::set_context( current_window, current_context );
 	}
 
 	void Renderer::render( const Application &app ) {
 		SDL_FNCHECK_V( OpenGL::set_context( m_window.m_handle, m_context ), return );
 
-		constexpr GLfloat verts[]
+		constexpr Vec2f verts[]
 		{
-			0.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f
+			{ 0.0f, 0.5f },
+			{ 0.0f, 1.0f },
+			{ 1.0f, 1.0f },
+			{ 0.0f, 0.0f },
+			{ -1.0f, 0.0f },
+			{ -1.0f, -1.0f },
 		};
 
+		const auto wnd_sz = m_window.size();
+		if (wnd_sz != m_state->last_window_size)
+			glViewport( -32, -32, wnd_sz.x, wnd_sz.y );
+
+		m_state->last_window_size = wnd_sz;
 
 		glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
 		glClear( GL_COLOR_BUFFER_BIT );
@@ -47,28 +81,57 @@ namespace route
 		GLuint vbo{ 0 };
 		GLuint vao{ 0 };
 
-		glGenVertexArrays( 1, &vao );
-		glGenBuffers( 1, &vbo );
-		glBindVertexArray( vao );
-		glBindBuffer( GL_ARRAY_BUFFER, vbo );
-		glBufferData( GL_ARRAY_BUFFER, sizeof( GLfloat ) * 6, verts, GL_STATIC_DRAW );
+		GL_CALL( glGenVertexArrays( 1, &vao ) );
+		GL_CALL( glGenBuffers( 1, &vbo ) );
+		GL_CALL( glBindVertexArray( vao ) );
+		GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, vbo ) );
+		GL_CALL( glBufferData( GL_ARRAY_BUFFER, sizeof( Vec2f ) * std::size( verts ), verts, GL_STATIC_DRAW ) );
 
-		glEnableVertexAttribArray( 0 );
-		glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof( GLfloat ), nullptr );
+		GL_CALL( glEnableVertexAttribArray( 0 ) );
+		GL_CALL( glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( Vec2f ), nullptr ) );
 
-		glBindBuffer( 0, vbo );
-		glBindVertexArray( 0 );
+		bind_shader( m_state->shader );
+		GL_CALL( glBindVertexArray( vao ) );
+		GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, vbo ) );
 
-		glBindVertexArray( vao );
-		glBindBuffer( GL_ARRAY_BUFFER, vbo );
-		glDrawArrays( GL_TRIANGLES, 0, 3 );
-		glBindBuffer( GL_ARRAY_BUFFER, 0 );
-		glBindVertexArray( 0 );
+		GL_CALL( glDrawArrays( GL_TRIANGLES, 0, std::size( verts ) ) );
 
-		glDeleteBuffers( 1, &vbo );
-		glDeleteVertexArrays( 1, &vao );
+		GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, 0 ) );
+		GL_CALL( glBindVertexArray( 0 ) );
+		clear_bound_shader();
+
+		GL_CALL( glDeleteBuffers( 1, &vbo ) );
+		GL_CALL( glDeleteVertexArrays( 1, &vao ) );
 
 		SDL_GL_SwapWindow( static_cast<LPSDLWindow>(m_window.m_handle) );
 	}
 
+	void Renderer::bind_shader( const Shader &shader ) {
+#ifdef GAPI_GL
+		GL_CALL( glUseProgram( (GLuint)shader.m_id ) );
+#endif
+	}
+
+	void Renderer::clear_bound_shader() {
+#ifdef GAPI_GL
+		glUseProgram( NULL );
+#endif
+	}
+
+
+#ifdef GAPI_GL
+	void Renderer::_do_command( const RenderCommandQueue::command_boxed &command ) {
+		using rcq::CommandType;
+		switch (command->type)
+		{
+		case CommandType::BindVertexSource:
+			{
+				const auto &c = command.get<rcq::CommandBindVertexSource>();
+				ResourceServer<StorageBuffer>::get_resource( c.vertex_buffer );
+			}
+		default:
+			break;
+		}
+	}
+#endif
 }
--- Route/src/Route/Renderer.h
+++ Route/src/Route/Renderer.h
@@ -1,205 +1,22 @@
 #pragma once
 #include "Scene.h"
 #include "Window.h"
-#include "ArrayView.h"
-#include "Primitives.h"
-#include "ResourceServer.h"
-#include "Texture.h"
-#include "Material.h"
-#include "Shader.h"
-#include "Vertex.h"
+#include "RenderCommandQueue.h"
 
 
 namespace route
 {
 	class Application;
 
-	enum class PrimitiveTopology
-	{
-		Triangles,
-		TriangleStrips,
-		TriangleFans,
-		Line,
-		LineStrips,
-		Points,
-		Patches,
-	};
 
-	enum class RendererAttribute
-	{
-		DepthTest,
-		// TODO: a lot of stuff (blending, scissors, ...)
-	};
-
-	enum class IndexType
-	{
-		Invalid = -1,
-		UInt8,
-		UInt16,
-		UInt32,
-	};
-
-	enum class CompareOperator
-	{
-		Never,
-		Less,
-		Equal,
-		LessOrEqual,
-		Greater,
-		NotEqual,
-		GreaterOrEqual,
-		Always
-	};
-
-	enum class CullMode
-	{
-		None,
-		Front,
-		Back,
-		FrontAndBack
-	};
-
-	enum class FaceWinding
-	{
-		CounterClockwise,
-		Clockwise,
-	};
 
 	class RenderCommandQueue
 	{
-		enum class CommandType
-		{
-			Draw,
-			DrawVertices,
-			BindVertexSource,
-			Transform2D,
-			Transform3D,
-			Texture,
-			NormalMap,
-			Metrial,
-			Shader,
-			Primitive2D,
-			Primitive3D,
-			Polygon,
-			Mesh,
-		};
-
-		enum class Filter
-		{
-			Nearest,
-			Linear,
-		};
-
-		enum class EdgeSampling
-		{
-			Repeate,
-			MirrorRepeate,
-			ClampToEdge,
-			ClampToBoarder
-		};
-
-		struct Command
-		{
-			inline constexpr Command( CommandType p_type )
-				: type{ p_type } {
-
-			}
-
-			const CommandType type;
-		};
-
-		template <typename _Ty, CommandType _Type>
-		struct TCommandSWrapper : public Command
-		{
-			using wrapped_type = _Ty;
-			inline TCommandSWrapper()
-
-				: Command{ _Type }, value{} {
-			}
-			inline TCommandSWrapper( const wrapped_type &p_value )
-				: Command{ _Type }, value{ p_value } {
-			}
-
-			wrapped_type value;
-		};
-
-		template <typename _Ty, CommandType _Type>
-		struct TCommandSSetResource : public Command
-		{
-			using resource_type = _Ty;
-
-			inline TCommandSSetResource()
-				: Command{ _Type }, rid{ RIDInvalid } {
-			}
-
-			inline TCommandSSetResource( RID p_rid )
-				: Command{ _Type }, rid{ p_rid } {
-			}
-
-			inline resource_type &resource() {
-				return ResourceServer<resource_type>::get_resource( rid );
-			}
-
-			inline const resource_type &resource() const {
-				return ResourceServer<resource_type>::get_resource( rid );
-			}
-
-			inline bool can_invalidate() const {
-				return ResourceServer<resource_type>::get_resource( rid ).change_counter() != change_counter;
-			}
-
-			RID rid;
-			// for cache invalidation
-			// if the resource has the same change counter, then it hasn't change
-			refc_t change_counter;
-		};
-
-		// vertex buffer state is cleared after drawing
-		struct CommandDrawVertices : public Command
-		{
-
-			inline CommandDrawVertices() : Command{ CommandType::DrawVertices } {
-			}
-
-			inline CommandDrawVertices( index_t p_offset, size_t p_count, PrimitiveTopology p_meshing_type )
-				: Command{ CommandType::DrawVertices }, offset{ p_offset }, count{ p_count }, meshing_type{ p_meshing_type } {
-			}
+	public:
+		using command_boxed = rcq::command_boxed;
 
 
-			// each vertex size is defined by the vertex input descriptor, see "VertexInputState"
-			index_t offset = 0;
-			size_t count = 0;
-			PrimitiveTopology meshing_type = PrimitiveTopology::TriangleStrips;
-		};
-
-		struct CommandBindVertexSourc : public Command
-		{
-			inline CommandBindVertexSourc() : Command{ CommandType::BindVertexSource } {
-			}
-
-			inline CommandBindVertexSourc( RID p_vertex_buffer, RID p_index_buffer, const Blob<const VertexInputState> &p_input_desc )
-				: Command{ CommandType::BindVertexSource },
-				vertex_buffer{ p_vertex_buffer }, index_buffer{ p_index_buffer }, input_desc{ p_input_desc } {
-			}
-
-			RID vertex_buffer;
-			RID index_buffer;
-			Blob<const VertexInputState> input_desc;
-		};
-
-
-		using CommandXForm2D = TCommandSWrapper<Transform2D, CommandType::Transform2D>;
-		using CommandXForm3D = TCommandSWrapper<Transform3D, CommandType::Transform3D>;
-
-		using CommandPrimitive2D = TCommandSWrapper<boxed_primitive_2d, CommandType::Primitive2D>;
-		using CommandPrimitive3D = TCommandSWrapper<boxed_primitive_3d, CommandType::Primitive3D>;
-
-		using CommandSetTexture = TCommandSWrapper<Texture, CommandType::Texture>;
-		using CommandSetMaterial = TCommandSWrapper<Material, CommandType::Metrial>;
-		using CommandSetShader = TCommandSWrapper<Shader, CommandType::Shader>;
-
-		// TODO: the type shifter
-		using command_boxed = boxed<Command, 128>;
+		vector<command_boxed> commands;
 	};
 
 	class GraphicsResourceFactory;
@@ -216,11 +33,18 @@ namespace route
 	private:
 		Renderer( const Renderer & ) = delete;
 		Renderer &operator =( const Renderer & ) = delete;
+
+		static void bind_shader( const Shader &shader );
+		static void clear_bound_shader();
+
+		void _do_command( const RenderCommandQueue::command_boxed &command );
+
 	private:
 		class State;
 		Window &m_window;
 		State *m_state;
 		void *m_context;
+		RenderCommandQueue m_command_queue;
 	};
 
 }
