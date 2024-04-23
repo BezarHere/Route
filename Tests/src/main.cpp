88f384f8cc7255f64ade7144a1e4db3ce910a7c4
botatodev <botatodev@gmail.com>
2024-04-23 16:54:12 +0300

    demo update

--- Tests/src/main.cpp
+++ Tests/src/main.cpp
@@ -51,25 +51,26 @@ volatile float val = 0.12;
 int main() {
 	constexpr bool ppp = std::is_assignable_v<route::object, route::object>;
 
-	{
-		imap<std::string> imap{};
-		std::map<int, std::string> map{};
+	//{
+	//	imap<std::string> imap{};
+	//	std::map<int, std::string> map{};
 
-		imap.add( 44, "hello" );
-		imap.add( 64, "x64" );
-		imap.add( 86, "x86" );
-		imap.add( 69, "nice" );
-		imap.add( 37, "so rand" );
-		imap.add( 42, "galaxy" );
-		imap.add( 44 * 2, "hello" );
-		imap.add( 64 * 2, "x64" );
-		imap.add( 86 * 2, "x86" );
-		imap.add( 69 * 2, "nice" );
-		imap.add( 37 * 2, "so rand" );
-		imap.add( 42 * 2, "galaxy" );
-	}
+	//	imap.add( 44, "hello" );
+	//	imap.add( 64, "x64" );
+	//	imap.add( 86, "x86" );
+	//	imap.add( 69, "nice" );
+	//	imap.add( 37, "so rand" );
+	//	imap.add( 42, "galaxy" );
+	//	imap.add( 44 * 2, "hello" );
+	//	imap.add( 64 * 2, "x64" );
+	//	imap.add( 86 * 2, "x86" );
+	//	imap.add( 69 * 2, "nice" );
+	//	imap.add( 37 * 2, "so rand" );
+	//	imap.add( 42 * 2, "galaxy" );
+	//}
 
-	Window window{ "hello", route::Rect2i( 100, 100, 200, 200 ) };
+	Window window{ "hello", route::Rect2i( 100, 100, 500, 500 ) };
+	//Window window2{ "hello2", route::Rect2i( 400, 100, 500, 500 ) };
 	Renderer renderer{ window };
 	route::Scene tree{};
 
@@ -82,12 +83,6 @@ int main() {
 	}
 
 	Application app{ window, renderer };
-
-	for (int i = 0; i < 10000; i++)
-	{
-		ResourceServer<Texture>::add_resource( Texture() );
-		ResourceServer<Texture>::add_resource( Texture() );
-	}
 
 	app.start();
 	app.stop();
