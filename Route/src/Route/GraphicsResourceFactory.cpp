13a447a4bc7e9abf7aa9a805c5b7979e734d8be6
botatodev <botatodev@gmail.com>
2024-05-03 22:44:56 +0300

    graphics resource factory changes

--- Route/src/Route/GraphicsResourceFactory.cpp
+++ Route/src/Route/GraphicsResourceFactory.cpp
@@ -1,12 +1,40 @@
 #include "pch.h"
 #include "GraphicsResourceFactory.h"
+#include "Renderer.h"
 
 namespace route
 {
+	GraphicsResourceFactory::GraphicsResourceFactory( Renderer &renderer )
+		: m_renderer{ renderer }, m_device{} {
 
-  RID GraphicsResourceFactory::create_buffer( StorageBufType type ) {
-    // TODO: stuff
-    return RIDInvalid;
-  }
+	}
+
+	void GraphicsResourceFactory::_create_begin() {
+		RT_ASSERT_JIC( is_locked() );
+		m_error = Error::Ok;
+	}
+
+	void GraphicsResourceFactory::_register( const GraphicsResource &res, RID rid ) {
+		(void)res;
+		m_resources.push_back( rid );
+	}
+
+	RID GraphicsResourceFactory::create_buffer( StorageBufType type ) {
+#ifdef GAPI_GL
+		StorageBufferID id = 0;
+		glGenBuffers(type)
+		ResourceServer<StorageBuffer>::add_resource();
+#endif
+		return RIDInvalid;
+	}
+
+	RID GraphicsResourceFactory::create_texture( const TextureInfo &info ) {
+		// TODO: stuff
+		return RIDInvalid;
+	}
+
+	bool GraphicsResourceFactory::is_locked() const {
+		return m_renderer.m_lock;
+	}
 
 }
--- Route/src/Route/GraphicsResourceFactory.h
+++ Route/src/Route/GraphicsResourceFactory.h
@@ -1,20 +1,39 @@
 #pragma once
 #include "ResourceServer.h"
 #include "StorageBuffer.h"
+#include "Texture.h"
 
 namespace route
 {
 	using DeviceID = int64_t;
 	typedef class GraphicsResourceFactory GphxResFac;
 
+	class Renderer;
+
 	class GraphicsResourceFactory
 	{
+		friend Renderer;
 	public:
 		RID create_buffer( StorageBufType type );
-		RID create_texture( StorageBufType type );
+		RID create_texture( const TextureInfo &info );
+
+		inline Error error() const {
+			return m_error;
+		}
+
+		bool is_locked() const;
 
 	private:
+		GraphicsResourceFactory( Renderer &renderer );
+
+		void _create_begin();
+		void _register( const GraphicsResource &res, RID rid );
+
+	private:
+		Renderer &m_renderer;
 		DeviceID m_device;
+		Error m_error;
+		vector<RID> m_resources;
 	};
 
 
