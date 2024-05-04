1bcf2c16b982244206e0a386d2540a78dc070748
botatodev <botatodev@gmail.com>
2024-05-04 05:26:00 +0300

    graphics res factory can now create storage buffers

--- Route/src/Route/GraphicsResourceFactory.cpp
+++ Route/src/Route/GraphicsResourceFactory.cpp
@@ -1,40 +1,41 @@
 #include "pch.h"
 #include "GraphicsResourceFactory.h"
 #include "Renderer.h"
+#include "../gapi.h"
+#include "internal/storage_buffer.h"
 
 namespace route
 {
-	GraphicsResourceFactory::GraphicsResourceFactory( Renderer &renderer )
-		: m_renderer{ renderer }, m_device{} {
+  GraphicsResourceFactory::GraphicsResourceFactory( Renderer &renderer )
+    : m_renderer{ renderer }, m_device{} {
 
-	}
+  }
 
-	void GraphicsResourceFactory::_create_begin() {
-		RT_ASSERT_JIC( is_locked() );
-		m_error = Error::Ok;
-	}
+  void GraphicsResourceFactory::_create_begin() {
+    RT_ASSERT_JIC( is_locked() );
+    m_error = Error::Ok;
+  }
 
-	void GraphicsResourceFactory::_register( const GraphicsResource &res, RID rid ) {
-		(void)res;
-		m_resources.push_back( rid );
-	}
+  void GraphicsResourceFactory::_register( const GraphicsResource &res, RID rid ) {
+    (void)res;
+    m_resources.push_back( rid );
+  }
 
-	RID GraphicsResourceFactory::create_buffer( StorageBufType type ) {
+  RID GraphicsResourceFactory::create_buffer( StorageBufType type ) {
 #ifdef GAPI_GL
-		StorageBufferID id = 0;
-		glGenBuffers(type)
-		ResourceServer<StorageBuffer>::add_resource();
+    GLuint id = 0;
+    glGenBuffers( _rt::storage_buffer::to_gl_type( type ), &id );
+    return  ResourceServer<StorageBuffer>::add_resource( StorageBuffer( (StorageBufferID)id ) );;
 #endif
-		return RIDInvalid;
-	}
+  }
 
-	RID GraphicsResourceFactory::create_texture( const TextureInfo &info ) {
-		// TODO: stuff
-		return RIDInvalid;
-	}
+  RID GraphicsResourceFactory::create_texture( const TextureInfo &info ) {
+    // TODO: stuff
+    return RIDInvalid;
+  }
 
-	bool GraphicsResourceFactory::is_locked() const {
-		return m_renderer.m_lock;
-	}
+  bool GraphicsResourceFactory::is_locked() const {
+    return m_renderer.m_lock;
+  }
 
 }
