f2aa8849f2fd977ca288d0b97c0b598144a74bcd
botatodev <botatodev@gmail.com>
2024-05-04 05:28:14 +0300

    renderer can now run again the sample scene

--- Route/src/Route/Renderer.cpp
+++ Route/src/Route/Renderer.cpp
@@ -3,141 +3,166 @@
 #include "Logger.h"
 #include "Renderer.h"
 #include "StorageBuffer.h"
+#include "Application.h"
 
-
-
+#include "internal/storage_buffer.h"
 
 namespace route
 {
-	class Renderer::State
-	{
-	public:
+  class Renderer::APIState
+  {
+  public:
 
-		State() {
-			auto vertex_shader =
-				"layout (location = 0) in vec4 pos; out vec3 clr;"
-				"void main() { gl_Position = pos; clr = vec3((pos.xy + vec2(1)) / 2.0, 0.5); }"
-				;
-			auto frag_shader =
-				"in vec3 clr; layout (location = 0) out vec4 Color;"
-				"void main() { Color = vec4(clr, 1.0); }"
-				;
-			shader = Shader( vertex_shader, frag_shader );
+    APIState() {
+      const auto vertex_shader =
+        "layout (location = 0) in vec4 pos; out vec3 clr;"
+        "void main() { gl_Position = pos; clr = vec3((pos.xy + vec2(1)) / 2.0, 0.5); }"
+        ;
+      const auto frag_shader =
+        "in vec3 clr; layout (location = 0) out vec4 Color;"
+        "void main() { Color = vec4(clr, 1.0); }"
+        ;
+      shader = ShaderProgram( Shader( vertex_shader, ShaderType::Vertex ), Shader( frag_shader, ShaderType::Fragment ) );
 
-		}
+    }
 
-		~State() {
-		}
+    ~APIState() {
+    }
 
-		Shader shader;
-		Vec2u last_window_size;
-	};
+    ShaderProgram shader;
+    Vec2u last_window_size;
+  };
 
-	Renderer::Renderer( Window &window )
-		: m_window{ window }, m_factory{ *this } {
-		// TODO: start OpenGL
-		m_context = OpenGL::create_context( static_cast<LPSDLWindow>(m_window.m_handle) );
-		if (!m_context)
-		{
-			Logger::write( "Failed to create context for Renderer", LogLevel::Error );
-			return;
-		}
+  Renderer::Renderer( Window &window )
+    : m_window{ window }, m_factory{ *this } {
+    // TODO: start OpenGL
+    m_context = OpenGL::create_context( static_cast<LPSDLWindow>(m_window.m_handle) );
+    if (!m_context)
+    {
+      Logger::write( "Failed to create context for Renderer", LogLevel::Error );
+      return;
+    }
 
-		m_state = new State{};
-	}
+    m_api_state = new APIState{};
+  }
 
-	Renderer::~Renderer() {
-		const auto current_context = OpenGL::get_context();
-		const auto current_window = OpenGL::get_context_window();
+  Renderer::~Renderer() {
+    const auto current_context = OpenGL::get_context();
+    const auto current_window = OpenGL::get_context_window();
 
-		OpenGL::set_context( m_window.m_handle, m_context );
-		delete m_state;
-		OpenGL::delete_context( m_context );
+    OpenGL::set_context( m_window.m_handle, m_context );
+    delete m_api_state;
+    OpenGL::delete_context( m_context );
 
-		OpenGL::set_context( current_window, current_context );
-	}
+    OpenGL::set_context( current_window, current_context );
+  }
 
-	void Renderer::render( const Application &app ) {
-		SDL_FNCHECK_V( OpenGL::set_context( m_window.m_handle, m_context ), return );
+  void Renderer::render( const Application &app ) {
+    SDL_FNCHECK_V( OpenGL::set_context( m_window.m_handle, m_context ), return );
 
-		constexpr Vec2f verts[]
-		{
-			{ 0.0f, 0.5f },
-			{ 0.0f, 1.0f },
-			{ 1.0f, 1.0f },
-			{ 0.0f, 0.0f },
-			{ -1.0f, 0.0f },
-			{ -1.0f, -1.0f },
-		};
+    constexpr Vec2f verts[]
+    {
+      { 0.0f, 0.5f },
+      { 0.0f, 1.0f },
+      { 1.0f, 1.0f },
+      { 0.0f, 0.0f },
+      { -1.0f, 0.0f },
+      { -1.0f, -1.0f },
+    };
 
-		const auto wnd_sz = m_window.size();
-		if (wnd_sz != m_state->last_window_size)
-			glViewport( -32, -32, wnd_sz.x, wnd_sz.y );
+    const auto wnd_sz = m_window.size();
+    if (wnd_sz != m_api_state->last_window_size)
+      glViewport( -32, -32, wnd_sz.x, wnd_sz.y );
 
-		m_state->last_window_size = wnd_sz;
+    m_api_state->last_window_size = wnd_sz;
 
-		glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
-		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT );
+    glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT );
 
-		GLuint vbo{ 0 };
-		GLuint vao{ 0 };
+    GLuint vbo{ 0 };
+    GLuint vao{ 0 };
 
-		GL_CALL( glGenVertexArrays( 1, &vao ) );
-		GL_CALL( glGenBuffers( 1, &vbo ) );
-		GL_CALL( glBindVertexArray( vao ) );
-		GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, vbo ) );
-		GL_CALL( glBufferData( GL_ARRAY_BUFFER, sizeof( Vec2f ) * std::size( verts ), verts, GL_STATIC_DRAW ) );
+    GL_CALL( glGenVertexArrays( 1, &vao ) );
+    GL_CALL( glGenBuffers( 1, &vbo ) );
+    GL_CALL( glBindVertexArray( vao ) );
+    GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, vbo ) );
+    GL_CALL( glBufferData( GL_ARRAY_BUFFER, sizeof( Vec2f ) * std::size( verts ), verts, GL_STATIC_DRAW ) );
 
-		GL_CALL( glEnableVertexAttribArray( 0 ) );
-		GL_CALL( glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( Vec2f ), nullptr ) );
+    GL_CALL( glEnableVertexAttribArray( 0 ) );
+    GL_CALL( glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( Vec2f ), nullptr ) );
 
-		bind_shader( m_state->shader );
-		GL_CALL( glBindVertexArray( vao ) );
-		GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, vbo ) );
+    set_shader_program( m_api_state->shader );
+    _bind_shader_program();
+    GL_CALL( glBindVertexArray( vao ) );
+    GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, vbo ) );
 
-		GL_CALL( glDrawArrays( GL_TRIANGLES, 0, std::size( verts ) ) );
+    GL_CALL( glDrawArrays( GL_TRIANGLES, 0, std::size( verts ) ) );
 
-		GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, 0 ) );
-		GL_CALL( glBindVertexArray( 0 ) );
-		clear_bound_shader();
+    GL_CALL( glBindBuffer( GL_ARRAY_BUFFER, 0 ) );
+    GL_CALL( glBindVertexArray( 0 ) );
+    m_state.ShaderProg = 0;
+    _bind_shader_program();
 
-		GL_CALL( glDeleteBuffers( 1, &vbo ) );
-		GL_CALL( glDeleteVertexArrays( 1, &vao ) );
+    GL_CALL( glDeleteBuffers( 1, &vbo ) );
+    GL_CALL( glDeleteVertexArrays( 1, &vao ) );
 
-		SDL_GL_SwapWindow( static_cast<LPSDLWindow>(m_window.m_handle) );
-	}
+    SDL_GL_SwapWindow( static_cast<LPSDLWindow>(m_window.m_handle) );
+  }
 
-	void Renderer::bind_shader( const Shader &shader ) {
+  void Renderer::set_buffer( const StorageBuffer &buffer ) {
+    m_state.Buffers[ static_cast<size_t>(buffer.get_type()) ] = buffer.get_id();
+  }
+
+  void Renderer::set_shader_program( const ShaderProgram &shader ) {
+    m_state.ShaderProg = shader.get_id();
+  }
+
+  void Renderer::_bind_buffer( const StorageBufType type ) const {
 #ifdef GAPI_GL
-		GL_CALL( glUseProgram( (GLuint)shader.m_id ) );
-#endif
-	}
+    GAPI_IF_GL( Application::graphics_api() == GraphicsAPI::OpenGL ) {
+      glBindBuffer( _rt::storage_buffer::to_gl_type( type ), _rt::to_gl_name( m_state.Buffers[ static_cast<size_t>(type) ] ) );
+      // TODO: check for errors?
+    }
+#endif // GAPI_GL
 
-	void Renderer::clear_bound_shader() {
-#ifdef GAPI_GL
-		glUseProgram( NULL );
-#endif
-	}
+  }
 
-	void Renderer::_bind_buffer( StorageBufferID buffer ) {
+  void Renderer::_bind_shader_program() const {
 #ifdef GAPI_GL
-#endif
-	}
+    GAPI_IF_GL( Application::graphics_api() == GraphicsAPI::OpenGL ) {
+      glUseProgram( _rt::to_gl_name( m_state.ShaderProg ) );
+      // TODO: check for errors?
+    }
+#endif // GAPI_GL
+  }
 
 #ifdef GAPI_GL
-	void Renderer::_do_command( const RenderCommandQueue::command_boxed &command ) {
-		using rcq::CommandType;
-		switch (command->type)
-		{
-		case CommandType::BindVertexSource:
-			{
-				const auto &c = command.get<rcq::CommandBindVertexSource>();
-				const auto &vertex_buf = ResourceServer<StorageBuffer>::get_resource( c.vertex_buffer );
-				RT_ASSERT_RELEASE( vertex_buf.type() == StorageBufType::Vertex );
-			}
-		default:
-			break;
-		}
-	}
+  void Renderer::_do_command_gl( const RenderCommandQueue::command_boxed &command ) {
+    using rcq::CommandType;
+    switch (command->type)
+    {
+    case CommandType::BindVertexSource:
+      {
+        const auto &c = command.get<rcq::CommandBindVertexSource>();
+        const auto &vertex_buf = ResourceServer<StorageBuffer>::get_resource( c.vertex_buffer );
+        RT_ASSERT_RELEASE( vertex_buf.get_type() == StorageBufType::Vertex );
+      }
+    default:
+      break;
+    }
+  }
 #endif
+
+#ifdef GAPI_VK
+  void Renderer::_do_command_vk( const RenderCommandQueue::command_boxed &command ) {
+    using rcq::CommandType;
+    switch (command->get_type)
+    {
+    case CommandType::BindVertexSource:
+    default:
+      break;
+    }
+}
+#endif
+
 }
--- Route/src/Route/Renderer.h
+++ Route/src/Route/Renderer.h
@@ -3,78 +3,83 @@
 #include "Window.h"
 #include "RenderCommandQueue.h"
 #include "GraphicsResourceFactory.h"
+#include "ShaderProgram.h"
 
 
 namespace route
 {
-	class Application;
+  class Application;
 
 
 
-	class RenderCommandQueue
-	{
-	public:
-		using command_boxed = rcq::command_boxed;
+  class RenderCommandQueue
+  {
+  public:
+    using command_boxed = rcq::command_boxed;
 
 
-		vector<command_boxed> commands;
-	};
+    vector<command_boxed> commands;
+  };
 
-	struct RendererState
-	{
-		array<StorageBufferID, (int)StorageBufType::_Max> Buffers{};
-		array<ShaderID, 3> Shaders{};
-	};
+  struct RendererState
+  {
+    array<StorageBufferID, (int)StorageBufType::_Max> Buffers{};
+    ShaderProgramID ShaderProg{};
+  };
 
-	typedef uint64_t RendererLock;
-	typedef void *RendererContext;
+  typedef uint64_t RendererLock;
+  typedef void *RendererContext;
 
-	class Renderer
-	{
-		friend GphxResFac;
-	public:
-		Renderer( Window &window );
-		~Renderer();
+  class Renderer
+  {
+    friend GphxResFac;
+  public:
+    Renderer( Window &window );
+    ~Renderer();
 
-		void render( const Application &app );
+    void render( const Application &app );
 
-		inline GphxResFac &get_factory() {
-			return m_factory;
-		}
+    inline GphxResFac &get_factory() {
+      return m_factory;
+    }
 
-		inline const GphxResFac &get_factory() const {
-			return m_factory;
-		}
+    inline const GphxResFac &get_factory() const {
+      return m_factory;
+    }
 
-		void bind_buffer( const StorageBuffer &buffer );
-		void bind_shader( const Shader &shader );
+    void set_buffer( const StorageBuffer &buffer );
+    void set_shader_program( const ShaderProgram &shader );
 
-		inline StorageBufferID get_bound_buffer( StorageBufType type ) const {
-			return m_state.Buffers[ static_cast<size_t>(type) ];
-		}
+    inline StorageBufferID get_bound_buffer( StorageBufType type ) const {
+      return m_state.Buffers[ static_cast<size_t>(type) ];
+    }
 
-		inline StorageBufferID get_bound_shader( ShaderType type ) const {
-			return m_state.Shaders[ static_cast<size_t>(type) ];
-		}
+    inline ShaderProgramID get_bound_shader_prog() const {
+      return m_state.ShaderProg;
+    }
 
-	private:
-		Renderer( const Renderer & ) = delete;
-		Renderer &operator =( const Renderer & ) = delete;
+  private:
+    Renderer( const Renderer & ) = delete;
+    Renderer &operator =( const Renderer & ) = delete;
 
-		void _do_command( const RenderCommandQueue::command_boxed &command );
+    void _bind_buffer( StorageBufType type) const;
+    void _bind_shader_program() const;
 
-	private:
-		class APIState;
-		RendererLock m_lock;
-		Window &m_window;
-		RendererState m_state;
+    void _do_command_gl( const RenderCommandQueue::command_boxed &command );
+    void _do_command_vk( const RenderCommandQueue::command_boxed &command );
 
-		RenderCommandQueue m_command_queue;
+  private:
+    class APIState;
+    RendererLock m_lock;
+    Window &m_window;
+    RendererState m_state;
 
-		RendererContext m_context;
-		APIState *m_api_state;
-		// the factory should always be the last member field, so it can be initialized after all other fields
-		GphxResFac m_factory;
-	};
+    RenderCommandQueue m_command_queue;
+
+    RendererContext m_context;
+    APIState *m_api_state;
+    // the factory should always be the last member field, so it can be initialized after all other fields
+    GphxResFac m_factory;
+  };
 
 }
