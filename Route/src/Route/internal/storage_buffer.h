5e42dc6f320c9502a2acbedb8cea0b3af3dfdb03
botatodev <botatodev@gmail.com>
2024-05-07 05:21:17 +0300

    add to_gl_primitive to storage_buffer.h

--- Route/src/Route/internal/storage_buffer.h
+++ Route/src/Route/internal/storage_buffer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../StorageBuffer.h"
+#include "../Enums.h"
 #include "../../gapi.h"
 
 namespace _rt
@@ -8,6 +9,22 @@ namespace _rt
 #ifdef GAPI_GL
   static inline constexpr GLuint to_gl_name(route::vpid_t id) {
     return GLuint(id);
+  }
+#endif
+
+
+#ifdef GAPI_GL
+  static inline constexpr GLuint to_gl_primitive(route::PrimitiveTopology topology) {
+    constexpr GLuint Lookup[] = {
+      GL_TRIANGLES,
+      GL_TRIANGLE_STRIP,
+      GL_TRIANGLE_FAN,
+      GL_LINES,
+      GL_LINE_STRIP,
+      GL_POINTS,
+      GL_PATCHES,
+    };
+    return Lookup[static_cast<size_t>(topology)];
   }
 #endif
 
