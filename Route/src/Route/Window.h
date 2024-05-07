fda6469376fc83ae8e1585ae8f9b4d8f71235251
botatodev <botatodev@gmail.com>
2024-05-07 05:23:28 +0300

    window.h tweaks and added get_handle

--- Route/src/Route/Window.h
+++ Route/src/Route/Window.h
@@ -3,51 +3,54 @@
 
 namespace route
 {
-	typedef void *WindowHandle;
-	typedef void *WindowContext;
+  typedef void *WindowHandle;
 
-	enum WindowFlags : unsigned
-	{
-		WindowFlag_Fullscreen = 0x00001,
-		WindowFlag_Hidden = 0x00008,
-		WindowFlag_Borderless = 0x0010,
-		WindowFlag_Resizable = 0x00020,
-		WindowFlag_Minimized = 0x00040,
-		WindowFlag_Maximized = 0x00080,
-		WindowFlag_AlwaysOnTop = 0x8000,
-	};
+  enum WindowFlags : unsigned
+  {
+    WindowFlag_Fullscreen = 0x00001,
+    WindowFlag_Hidden = 0x00008,
+    WindowFlag_Borderless = 0x0010,
+    WindowFlag_Resizable = 0x00020,
+    WindowFlag_Minimized = 0x00040,
+    WindowFlag_Maximized = 0x00080,
+    WindowFlag_AlwaysOnTop = 0x8000,
+  };
 
-	class Renderer;
+  class Renderer;
 
-	class Window
-	{
-		friend Renderer;
-	public:
+  class Window
+  {
+    friend Renderer;
+  public:
 
-		Window( const std::string &title,
-						const Rect2i &frame,
-						WindowFlags flags = WindowFlag_Resizable );
-		~Window();
+    Window(const std::string &title,
+           const Rect2i &frame,
+           WindowFlags flags = WindowFlag_Resizable);
+    ~Window();
 
-		Window( Window & );
-		Window &operator=( Window & );
-		Window( const Window & ) = delete;
-		Window &operator=( const Window & ) = delete;
+    Window(Window &);
+    Window &operator=(Window &);
+    Window(const Window &) = delete;
+    Window &operator=(const Window &) = delete;
 
-		Vec2u size() const;
+    Vec2u size() const;
 
-		void poll();
-		inline operator bool() const {
-			return !m_should_close;
-		}
+    void poll();
+    inline operator bool() const {
+      return !m_should_close;
+    }
 
-	private:
+    inline WindowHandle get_handle() const {
+      return m_handle;
+    }
 
-		void _initialize();
+  private:
 
-	private:
-		WindowHandle m_handle;
-		bool m_should_close;
-	};
+    void _initialize();
+
+  private:
+    WindowHandle m_handle;
+    bool m_should_close;
+  };
 
 }
