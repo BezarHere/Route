b42bf20ce2d3e747cc686de15b8c242a24d2d8ca
botatodev <botatodev@gmail.com>
2024-05-10 06:00:34 +0300

    traits update, added alias for vector and lower_vector

--- Route/src/Route/Traits.h
+++ Route/src/Route/Traits.h
@@ -6,32 +6,36 @@
 
 namespace route
 {
-	namespace traits
-	{
-		class BaseTrait
-		{
-		private:
-			// can't be constructed/deleted
-			BaseTrait() = delete;
-			~BaseTrait() = delete;
-		};
+  namespace traits
+  {
+    class BaseTrait
+    {
+    private:
+      // can't be constructed/deleted
+      BaseTrait() = delete;
+      ~BaseTrait() = delete;
+    };
 
-		class Impl2D : BaseTrait
-		{
-		public:
-			using transform = Transform2D;
-			using direction = typename transform::vector_type;
-			using zindex = int16_t;
-		};
+    class Impl2D : BaseTrait
+    {
+    public:
+      using transform = Transform2D;
+      using direction = typename transform::vector_type;
+      using vector = typename transform::vector_type;
+      using lower_vector = typename vector::lower_type;
+      using zindex = int16_t;
+    };
 
-		class Impl3D : BaseTrait
-		{
-		public:
-			using transform = Transform3D;
-			using direction = typename transform::vector_type;
-			using zindex = nothing;
-		};
-	}
+    class Impl3D : BaseTrait
+    {
+    public:
+      using transform = Transform3D;
+      using direction = typename transform::vector_type;
+      using vector = typename transform::vector_type;
+      using lower_vector = typename vector::lower_type;
+      using zindex = nothing;
+    };
+  }
 }
 
 #pragma warning(pop)
